e629231db49ab003c0b9f4cd3e41d09d90038238
spiroskonstantinidis <60156291+spiros-k@users.noreply.github.com>
2024-06-16 22:19:37 +0300

    complete model, start view

--- app.js
+++ app.js
@@ -1,54 +1,92 @@
 
 class Model {
     constructor() {
-
         // things already have in my list
-        this.toDoItmes = [
+        this.toDoItems = [
             {id: 1, text: "Wash the dishes", completed: false},
+            {id: 2, text: "Wash the dog", completed: false},
         ]
     }
 
     addToDoItems(todoText) {
-        if(this.toDoItmes != []){
+        
+        if(this.toDoItems != []){
             const item = {
-                id: this.toDoItmes[this.toDoItmes.length].id + 1, 
+                id: this.toDoItems[this.toDoItems.length - 1].id + 1, 
                 text: todoText, 
                 completed: false
             }
-            this.toDoItmes.push(item)
+            this.toDoItems.push(item)
         } else {
             const item = {
                 id: 1,
                 text: todoText,
                 completed: false
             }
-            this.toDoItmes.push(item)
+            this.toDoItems.push(item)
         }
     }
 
     editToDoItems(id, textNew) {
-        this.toDoItmes.forEach((item) => {
-            if(this.toDoItmes.id === id) {
+        this.toDoItems.forEach((item) => {
+            if(this.toDoItems.id === id) {
                 item = {
-                    id: this.toDoItmes.id,
+                    id: this.toDoItems.id,
                     text: textNew,
                     completed: false
                 }
-                this.toDoItmes.push(item)
+                this.toDoItems.push(item)
             }
         })
     }
 
     deleteToDoItems(id) {
-        this.toDoItmes.filter((item) => {
+        this.toDoItems.filter((item) => {
             item.id !== id
+        })
+    }
+
+    toggleToDoItems(id) {
+        this.toDoItems.map((item) => {
+            if(item.id === id){
+                item = {
+                    id: item.id,
+                    text: item.text,
+                    completed: !item.completed
+                }
+                this.toDoItems.push(item)
+            }
         })
     }
 }
 
 class View {
     constructor() {
+        const titleHeader = document.createElement("h1")
+        let headerContent = document.createTextNode("Things To-Do")
+        
+        titleHeader.appendChild(headerContent)
 
+        this.div = document.createElement("div")
+        this.form = document.createElement("div")
+
+        this.input = document.createElement("input")
+        this.input.type = "text"
+        this.input.placeholder = "Add an item"
+        this.input.name = "todo-item"
+
+        this.submit = document.createElement("button")
+
+        const submitText = document.createTextNode("Submit")
+        this.submit.appendChild(submitText)
+
+        this.form.classList.add("flex-gap")
+        this.form.append(this.input, this.submit)
+        this.div.classList.add("flex")
+        this.div.append(titleHeader, this.form)
+
+        const currentRoot = document.getElementById("root")
+        currentRoot.appendChild(this.div)
     }
 }
 
@@ -60,3 +98,5 @@ class Controller {
 }
 
 const app = new Controller(new Model(), new View())
+app.model.addToDoItems("Read one chapter")
+console.log(app)
